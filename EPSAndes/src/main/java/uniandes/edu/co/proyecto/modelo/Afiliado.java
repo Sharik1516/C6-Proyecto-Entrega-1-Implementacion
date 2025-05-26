66068682f843176c346a122ab9a328d937ac5fc1
Martin Leschhorn <m.leschhorn@uniandes.edu.co>
2025-05-25 23:44:27 -0500

    Cambio relacion

--- EPSAndes/src/main/java/uniandes/edu/co/proyecto/modelo/Afiliado.java
+++ EPSAndes/src/main/java/uniandes/edu/co/proyecto/modelo/Afiliado.java
@@ -1,59 +1,68 @@
 package uniandes.edu.co.proyecto.modelo;
 
-import java.sql.Date;
-import jakarta.persistence.Entity;
-import jakarta.persistence.PrimaryKeyJoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "afiliados")
 @PrimaryKeyJoinColumn(name = "idUsuario")
 public class Afiliado extends Usuario {
 
+    private String nombre;
+    private String tipoDocumento;
+    private String numeroDocumento;
     private Date fechaNacimiento;
     private String direccion;
     private String telefono;
     private String tipoAfiliado;
 
+    @ManyToOne
+    @JoinColumn(name = "id_eps")
+    private EPS eps;
+
+    @ManyToOne
+    @JoinColumn(name = "nit_ips")
+    private IPS ips;
+
     public Afiliado() {}
 
-    public Afiliado(String nombre, String tipoDocumento, String numeroDocumento, Date fechaNacimiento, String direccion, String telefono, String tipoAfiliado) {
+    public Afiliado(String nombre, String tipoDocumento, String numeroDocumento,
+                    Date fechaNacimiento, String direccion, String telefono,
+                    String tipoAfiliado, EPS eps, IPS ips) {
         super(nombre, tipoDocumento, numeroDocumento);
         this.fechaNacimiento = fechaNacimiento;
         this.direccion = direccion;
         this.telefono = telefono;
         this.tipoAfiliado = tipoAfiliado;
+        this.eps = eps;
+        this.ips = ips;
     }
 
-    public Date getFechaNacimiento() {
-        return fechaNacimiento;
-    }
 
-    public void setFechaNacimiento(Date fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public String getDireccion() {
-        return direccion;
-    }
+    public String getTipoDocumento() { return tipoDocumento; }
+    public void setTipoDocumento(String tipoDocumento) { this.tipoDocumento = tipoDocumento; }
 
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
+    public String getNumeroDocumento() { return numeroDocumento; }
+    public void setNumeroDocumento(String numeroDocumento) { this.numeroDocumento = numeroDocumento; }
 
-    public String getTelefono() {
-        return telefono;
-    }
+    public Date getFechaNacimiento() { return fechaNacimiento; }
+    public void setFechaNacimiento(Date fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
 
-    public String getTipoAfiliado() {
-        return tipoAfiliado;
-    }
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
 
-    public void setTipoAfiliado(String tipoAfiliado) {
-        this.tipoAfiliado = tipoAfiliado;
-    }
+    public String getTipoAfiliado() { return tipoAfiliado; }
+    public void setTipoAfiliado(String tipoAfiliado) { this.tipoAfiliado = tipoAfiliado; }
+
+    public EPS getEps() { return eps; }
+    public void setEps(EPS eps) { this.eps = eps; }
+
+    public IPS getIps() { return ips; }
+    public void setIps(IPS ips) { this.ips = ips; }
 }
