6b2fd099e33abad1e4dda78a7061a577ae61c0cd
Juan Esteban <je.rodriguez23@uniandes.edu.co>
2025-05-25 20:30:51 -0500

    beta

--- EPSAndes/src/main/java/uniandes/edu/co/proyecto/modelo/Afiliado.java
+++ EPSAndes/src/main/java/uniandes/edu/co/proyecto/modelo/Afiliado.java
@@ -1,28 +1,67 @@
 package uniandes.edu.co.proyecto.modelo;
 
-import java.sql.Date;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.util.Date;
 
-@Entity
-@Table(name="afiliados")
-public class Afiliado extends Usuario {
+@Document(collection = "afiliados")
+public class Afiliado {
 
+    @Id
+    private String idAfiliado;
+
+    private String nombre;
+    private String tipoDocumento;
+    private String numeroDocumento;
     private Date fechaNacimiento;
     private String direccion;
     private String telefono;
     private String tipoAfiliado;
 
+    public Afiliado() {}
+
     public Afiliado(String nombre, String tipoDocumento, String numeroDocumento, Date fechaNacimiento, String direccion, String telefono, String tipoAfiliado) {
-        super(nombre, tipoDocumento, numeroDocumento);
+        this.nombre = nombre;
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
         this.fechaNacimiento = fechaNacimiento;
         this.direccion = direccion;
         this.telefono = telefono;
         this.tipoAfiliado = tipoAfiliado;
     }
 
-    public Afiliado() {}
+    public String getIdAfiliado() {
+        return idAfiliado;
+    }
+
+    public void setIdAfiliado(String idAfiliado) {
+        this.idAfiliado = idAfiliado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
 
     public Date getFechaNacimiento() {
         return fechaNacimiento;
